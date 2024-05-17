3959c14a0ac3006bb038e3dec45e1dc23f7fac01
CakZemprongzz <fauzanrizqimuhammad@gmail.com>
2024-05-17 11:14:09 +0700

    Added Shell script for updating the container

---
+++ update-container.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+git pull
+
+podman build -t budget-buddy-php .
+
+podman stop budget-buddy-server
+
+podman rm budget-buddy-server
+
+podman run -d -p 8080:8080 --name budget-buddy-server budget-buddy-php
+
+echo "Deployment complete. The application is running on port 8080."
