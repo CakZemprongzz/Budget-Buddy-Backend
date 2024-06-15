ca43f40dccebd4a5051eb5e956e24f7e1fa0b582
CakZemprongzz <fauzanrizqimuhammad@gmail.com>
2024-06-15 10:54:56 +0700

    updated script to make it cleaner

--- rebuild-container.sh
+++ rebuild-container.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
-podman build -t budget-buddy-php .
-
 podman stop budget-buddy-server
 
 podman rm budget-buddy-server
+
+podman rmi budget-buddy-php
+
+podman build -t budget-buddy-php .
+
+
 
 podman run -d -p 8080:8080 --name budget-buddy-server budget-buddy-php
 
--- update-container.sh
+++ update-container.sh
@@ -2,11 +2,13 @@
 
 git pull
 
-podman build -t budget-buddy-php .
-
 podman stop budget-buddy-server
 
 podman rm budget-buddy-server
+
+podman rmi budget-buddy-php
+
+podman build -t budget-buddy-php .
 
 podman run -d -p 8080:8080 --name budget-buddy-server budget-buddy-php
 
